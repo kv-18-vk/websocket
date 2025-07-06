073f38231814e32270f4bffcc34155355b7881d0
kv-18-vk <cse240001040@iiti.ac.in>
2025-07-06 12:48:55 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,29 @@
+const WebSocket = require('ws');
+const server = new WebSocket.Server({ port: 3000 });
+
+let waitingPlayer = null;
+
+server.on('connection', user => {
+  console.log("A player connected.");
+
+  user.on('close',()=>{
+    console.log("player disconnected");
+  })
+
+  if(!waitingPlayer){
+    waitingPlayer = user;
+  }else{
+    player1 = waitingPlayer;
+    player2 = user;
+    waitingPlayer = null;
+
+    player1.send(JSON.stringify({type:"status",msg:"Player Connected"}));
+    player2.send(JSON.stringify({type:"status",msg:"Player Connected"}));
+    
+
+  }
+
+});
+
+
+
