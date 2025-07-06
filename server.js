40b6b7a7913a1ef196d48e42b9ed5f62700bff8c
kv-18-vk <cse240001040@iiti.ac.in>
2025-07-06 22:42:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -55,7 +55,7 @@ wss.on('connection', user => {
   }
 });
 
-// Render sets PORT via env variable
+
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
